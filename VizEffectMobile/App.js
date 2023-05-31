16d173a3ea8bb2cf1b4ec0d71b078c8ac41028d8
Endre Svendsen <ensv001@student.kristiania.no>
2023-05-31 12:02:11 +0200

    Login

--- VizEffectMobile/App.js
+++ VizEffectMobile/App.js
@@ -1,21 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import LoginScreen from "./Pages/LoginScreen";
+import LoginScreen from "./pages/LoginScreen";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <LoginScreen />
-      <StatusBar style="auto" />
-    </View>
+      <View style={{flex: 1}}>
+        <LoginScreen />
+      </View>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
