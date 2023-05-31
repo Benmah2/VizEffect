83e5a60791cf6abe0f883d926cfc98450acd37ba
Endre Svendsen <ensv001@student.kristiania.no>
2023-05-31 12:22:42 +0200

    Test text in magenta color

---
+++ VizEffectMobile/pages/LoginScreen.js
@@ -0,0 +1,14 @@
+import {LinearGradient} from 'expo-linear-gradient';
+import {View, Text, StyleSheet, Image, SafeAreaView, TextInput, Dimensions, Button, ViewComponent} from "react-native";
+
+const {height} = Dimensions.get("window");
+
+const LoginScreen = () => {
+    return (
+        <LinearGradient colors={["#192821", "#261213"]} style={{height: "100%"}}>
+            <Text style={{color: "magenta"}}>Hello!</Text>
+        </LinearGradient>
+    );
+}
+
+export default LoginScreen;
