68feb6df511e859ec2101d757f6ad291717be488
Endre Svendsen <ensv001@student.kristiania.no>
2023-05-31 11:42:04 +0200

    Added lineargradien backgroundcolor

---
+++ VizEffectMobile/pages/LoginScreen.js
@@ -0,0 +1,11 @@
+import {LinearGradient} from 'expo-linear-gradient';
+
+const LoginScreen = () => {
+    return (
+        <LinearGradient colors={["#192821", "#261213"]} style={{height: "100%"}}>
+
+        </LinearGradient>
+    );
+}
+
+export default LoginScreen;
