183837d4a4c05f18376308804f8abc2fe29c0b59
Endre Svendsen <ensv001@student.kristiania.no>
2023-05-31 11:44:38 +0200

    Added lineargradien backgroundcolor

--- VizEffectMobile/App.js
+++ VizEffectMobile/App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import Login from "./Pages/LoginScreen";
+import LoginScreen from "./Pages/LoginScreen";
 
 export default function App() {
   return (
