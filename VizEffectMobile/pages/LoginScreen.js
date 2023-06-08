ba57bca29c728a8608de291a65a00817c682b325
Endre Svendsen <ensv001@student.kristiania.no>
2023-06-08 11:13:45 +0200

    Endret litt på loginscreen

--- VizEffectMobile/pages/LoginScreen.js
+++ VizEffectMobile/pages/LoginScreen.js
@@ -6,13 +6,10 @@ import {
     StyleSheet,
     Image,
     TextInput,
-    Dimensions,
     TouchableOpacity,
 } from 'react-native';
 import AntDesign from 'react-native-vector-icons/AntDesign';
 import Ionicons from 'react-native-vector-icons/Ionicons';
-
-const { height } = Dimensions.get('window');
 
 const LoginScreen = ({ navigation }) => {
     const [email, setEmail] = useState('');
@@ -21,145 +18,178 @@ const LoginScreen = ({ navigation }) => {
     const [passwordError, setPasswordError] = useState('');
 
     const handleLogin = () => {
-        // Reset previous error messages
         setEmailError('');
         setPasswordError('');
 
-        // Validation logic
-        if (email === '') {
+        if (!email) {
             setEmailError('Email is required');
-        }
-        if (password === '') {
-            setPasswordError('Password is required');
+        } else if (!/\S+@\S+\.\S+/.test(email)) {
+            setEmailError('Invalid email format');
         }
 
-        // Perform login logic if validation passes
-        if (email !== '' && password !== '') {
+        if (!password) {
+            setPasswordError('Password is required');
+        } else if (password.length < 8) {
+            setPasswordError('Password must be at least 8 characters long');
+        } else if (!/[!@#$%^&*(),.?":{}|<>]/.test(password)) {
+            setPasswordError('Password must contain at least one special character');
+        }
+
+        if (!emailError && !passwordError) {
             navigation.navigate('Landing');
         }
     };
 
     return (
-        <LinearGradient colors={['#192821', '#261213']} style={{ height: '100%' }}>
-            <View style={styles.container}>
-                <View style={{ marginTop: 30, flexDirection: 'row', marginBottom: 20, paddingTop: 20 }}>
-                    <Image resizemode="contain" source={require('../assets/images/logo.png')} style={styles.logoBilde} />
-                </View>
-                <View style={styles.logoContainer}>
-                    <Image resizemode="contain" source={require('../assets/images/loginbilde.png')} style={styles.logo} />
-                </View>
-
-                {/* Email input */}
-                <Text style={{ fontSize: 18, fontWeight: '500', color: '#E1E1E1', marginBottom: 20 }}>Email</Text>
-                <View style={[styles.inputContainer, emailError && styles.errorInputContainer]}>
-                    <AntDesign name="mail" size={30} style={{ color: '#E1E1E1', marginRight: 10, marginLeft: 10 }} />
-                    <TextInput
-                        placeholder="Email address"
-                        style={[styles.input, emailError && styles.errorInput]}
-                        keyboardType="email-address"
-                        value={email}
-                        onChangeText={setEmail}
-                    />
-                </View>
-                {emailError ? <Text style={styles.errorText}>{emailError}</Text> : null}
-
-                {/* Password input */}
-                <Text style={{ fontSize: 18, fontWeight: '500', color: '#E1E1E1', marginBottom: 20 }}>Password</Text>
-                <View style={[styles.inputContainer, passwordError && styles.errorInputContainer]}>
-                    <Ionicons
-                        name="ios-lock-closed-outline"
-                        size={30}
-                        style={{ color: '#E1E1E1', marginRight: 10, marginLeft: 10 }}
-                    />
-                    <TextInput
-                        placeholder="Password"
-                        style={[styles.input, passwordError && styles.errorInput]}
-                        secureTextEntry={true}
-                        value={password}
-                        onChangeText={setPassword}
-                    />
-                </View>
-                {passwordError ? <Text style={styles.errorText}>{passwordError}</Text> : null}
-
-                {/* Forgot password btn */}
-                <TouchableOpacity style={{ flexDirection: 'row', left: 252, marginBottom: 40 }} onPress={() => {}}>
-                    <Text style={{ paddingRight: 5, color: '#D9574A', fontSize: 12 }}>Forgot</Text>
-                    <Text style={{ color: '#E1E1E1', fontSize: 12 }}>Password</Text>
-                </TouchableOpacity>
-
-                {/* Login btn */}
-                <LinearGradient style={{ marginBottom: 20, borderRadius: 10 }} colors={['#EA7F74', '#D9574A']}>
-                    <TouchableOpacity style={styles.button} onPress={handleLogin}>
-                        <Text style={{ paddingRight: 5, color: '#E1E1E1', fontSize: 15, fontWeight: '700', textAlign: 'center' }}>
-                            Login
-                        </Text>
-                    </TouchableOpacity>
-                </LinearGradient>
-
-                <Text style={{ marginBottom: 20, textAlign: 'center', color: '#E1E1E1', fontSize: 12 }}>----- Or -----</Text>
-
-                {/* Sign up btn */}
-                <LinearGradient style={{ marginBottom: 30, borderRadius: 10 }} colors={['#EA7F74', '#D9574A']}>
-                    <TouchableOpacity style={styles.button} onPress={() => {}}>
-                        <Text style={{ paddingRight: 5, color: '#E1E1E1', fontSize: 15, fontWeight: '700', textAlign: 'center' }}>
-                            Sign up
-                        </Text>
-                    </TouchableOpacity>
-                </LinearGradient>
+        <LinearGradient colors={['#192821', '#261213']} style={styles.container}>
+            <View style={styles.logoContainer}>
+                <Image
+                    source={require('../assets/images/logo.png')}
+                    style={styles.logoImage}
+                    resizeMode="contain"
+                />
             </View>
+
+            <View style={styles.logoContainer2}>
+                <Image
+                    source={require('../assets/images/loginbilde.png')}
+                    style={styles.logo}
+                    resizeMode="contain"
+                />
+            </View>
+
+            <Text style={styles.label}>Email</Text>
+            <View style={[styles.inputContainer, emailError && styles.errorInputContainer]}>
+                <AntDesign name="mail" size={30} style={styles.icon} />
+                <TextInput
+                    placeholder="Email address"
+                    style={[styles.input, emailError && styles.errorInput]}
+                    keyboardType="email-address"
+                    value={email}
+                    onChangeText={setEmail}
+                />
+            </View>
+            {emailError && <Text style={styles.errorText}>{emailError}</Text>}
+
+            <Text style={styles.label}>Password</Text>
+            <View style={[styles.inputContainer, passwordError && styles.errorInputContainer]}>
+                <Ionicons name="ios-lock-closed-outline" size={30} style={styles.icon} />
+                <TextInput
+                    placeholder="Password"
+                    style={[styles.input, passwordError && styles.errorInput]}
+                    secureTextEntry
+                    value={password}
+                    onChangeText={setPassword}
+                />
+            </View>
+            {passwordError && <Text style={styles.errorText}>{passwordError}</Text>}
+
+            <TouchableOpacity style={styles.forgotPasswordBtn} onPress={() => {}}>
+                <Text style={styles.forgotPasswordText}>Forgot Password</Text>
+            </TouchableOpacity>
+
+            <LinearGradient style={styles.button} colors={['#EA7F74', '#D9574A']}>
+            <TouchableOpacity onPress={handleLogin}>
+                <Text style={styles.buttonText}>Login</Text>
+            </TouchableOpacity>
+            </LinearGradient>
+
+            <Text style={styles.orText}>----- Or -----</Text>
+
+            <LinearGradient style={styles.button} colors={['#EA7F74', '#D9574A']}>
+            <TouchableOpacity>
+                <Text style={styles.buttonText}>Sign up</Text>
+            </TouchableOpacity>
+            </LinearGradient>
         </LinearGradient>
     );
 };
 
 const styles = StyleSheet.create({
     container: {
+        flex: 1,
         paddingHorizontal: 25,
+        justifyContent: 'center',
     },
     logoContainer: {
-        alignItems: 'center',
+        flexDirection: 'row',
+        justifyContent: 'flex-start',
+        marginBottom: 20,
     },
-    image: {
-        top: 40,
-        width: 150,
+    logoContainer2: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        marginBottom: 20,
+    },
+    logoImage: {
+        width: 130,
         height: 25,
+        marginTop: 30,
+        marginBottom: 20,
+        paddingTop: 20,
     },
     logo: {
         width: 300,
         height: 300,
-        marginBottom: 5,
     },
-    logoBilde: {
-        width: 130,
-        height: 25,
+    label: {
+        fontSize: 18,
+        fontWeight: '500',
+        color: '#E1E1E1',
+        marginBottom: 10,
     },
     inputContainer: {
         flexDirection: 'row',
         alignItems: 'center',
-        paddingTop: 8,
-        paddingBottom: 8,
+        paddingHorizontal: 10,
         marginBottom: 20,
         backgroundColor: 'rgba(92, 88, 87, 0.28)',
         borderRadius: 10,
         height: 60,
     },
     errorInputContainer: {
-        borderColor: "red",
+        borderColor: 'red',
         borderWidth: 1,
     },
     input: {
         flex: 1,
-        paddingHorizontal: 0,
-        color: "white",
+        color: 'white',
+    },
+    icon: {
+        color: '#E1E1E1',
+        marginHorizontal: 5,
     },
     errorInput: {
-        color: "red",
+        color: 'red',
     },
     errorText: {
-        color: "red",
+        color: 'red',
         marginBottom: 10,
     },
+    forgotPasswordBtn: {
+        alignSelf: 'flex-end',
+        marginBottom: 40,
+    },
+    forgotPasswordText: {
+        color: '#D9574A',
+        fontSize: 12,
+    },
     button: {
-        padding: 15,
+        paddingVertical: 15,
+        marginBottom: 20,
+        borderRadius: 10,
+    },
+    buttonText: {
+        color: '#252525',
+        fontSize: 18,
+        fontWeight: 'bold',
+        textAlign: 'center',
+    },
+    orText: {
+        marginBottom: 20,
+        textAlign: 'center',
+        color: '#E1E1E1',
+        fontSize: 12,
     },
 });
 
